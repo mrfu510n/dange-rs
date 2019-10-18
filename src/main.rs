c44ba8efbd3e123e44437d0c2563ebf48af58201
MrFusion <mrcoinfusion@gmail.com>
2019-10-17 21:48:16 -0500

    Print all processes

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 #[cfg(windows)] extern crate winapi;
 use std::io::Error;
 
-use winapi::um::tlhelp32::{CreateToolhelp32Snapshot, TH32CS_SNAPPROCESS, Process32First, PROCESSENTRY32};
+use winapi::um::tlhelp32::{CreateToolhelp32Snapshot, TH32CS_SNAPPROCESS, Process32First, Process32Next, PROCESSENTRY32};
 use winapi::um::handleapi::{CloseHandle, INVALID_HANDLE_VALUE};
-use winapi::um::winnt::{HANDLE};
-use winapi::shared::minwindef::{TRUE, MAX_PATH};
+use winapi::um::winnt::{HANDLE, PROCESS_ALL_ACCESS};
+use winapi::um::processthreadsapi::{GetPriorityClass, OpenProcess};
+use winapi::shared::minwindef::{TRUE, FALSE, MAX_PATH};
 
 // Taking a Snapshot and Viewing Processes
 // https://docs.microsoft.com/en-us/windows/win32/toolhelp/taking-a-snapshot-and-viewing-processes
@@ -25,21 +26,23 @@ fn get_process_list() -> Result<(), Error> {
         //   If you do not initialize dwSize, Process32First fails.
         process.dwSize = std::mem::size_of::<PROCESSENTRY32>() as u32;
 
-        let found = unsafe {
-            Process32First(snapshot_handle, &mut process)
-        };
+        if unsafe { Process32First(snapshot_handle, &mut process) } == TRUE {
+            print_process(&process);
 
-        if found == TRUE {
-            let name = process_name(&process);
-            println!("name: {:?}\nid: {:?}", name, process.th32ProcessID);
+            while unsafe { Process32Next(snapshot_handle, &mut process) } == TRUE {
+                print_process(&process);
+            }
+
             unsafe {
                 CloseHandle(snapshot_handle);
             }
+
             Ok(())
         } else {
             unsafe {
                 CloseHandle(snapshot_handle);
             }
+
             Err(Error::last_os_error())
         }
     }
@@ -48,6 +51,24 @@ fn get_process_list() -> Result<(), Error> {
 fn get_process_list(name: &str) -> Result<(), Error> {
     println!("can't find process named '{}'", name);
     Ok(())
+}
+
+fn print_process(process: &PROCESSENTRY32) -> () {
+    let name = process_name(&process);
+    let priority_class = {
+        unsafe {
+            let h_process = OpenProcess(PROCESS_ALL_ACCESS, FALSE, process.th32ProcessID);
+            GetPriorityClass(h_process)
+        }
+    };
+    println!("=======================================================");
+    println!("PROCESS NAME:  {}", name);
+    println!("-------------------------------------------------------");
+    println!("  Process ID        = 0x{:x}", process.th32ProcessID);
+    println!("  Thread count      = {}", process.cntThreads);
+    println!("  Parent process ID = 0x{:x}", process.th32ParentProcessID);
+    println!("  Priority base     = {}", process.pcPriClassBase);
+    println!("  Priority class    = {}", priority_class);
 }
 
 fn process_name(process: &PROCESSENTRY32) -> &str {
